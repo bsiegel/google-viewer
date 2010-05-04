186f2091cf8c47ab7a4b315ad2b87661976c4900
Brandon Siegel <brandon@smartercode.net>
2010-05-04 15:27:57 -0400

    Reorganize and add auto-build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJDIR=$(cd `dirname $0` && pwd)
+PROJNAME=`basename $PROJDIR`
+COMMITDATE=$(cd $PROJDIR && hg tip --template "{date|shortdate}")
+XPIFILE=$(cd $PROJDIR && hg tip --template "$PROJNAME-rev{rev}-{node|short}.xpi")
+BUILDDIR=$PROJDIR/builds/$COMMITDATE
+
+mkdir -p $BUILDDIR
+rm -f $BUILDDIR/$XPIFILE
+cd $PROJDIR/src
+zip -q -r $BUILDDIR/$XPIFILE .
